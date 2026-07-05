4c2beb4d7b0e7d54f8b88ef257bc7cc414a38250
afaq <m.afaqpak@gmail.com>
2026-07-05 09:33:10 +0500

    vercel deploy ready

--- app/database.py
+++ app/database.py
@@ -1,9 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./travel.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./travel.db")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in SQLALCHEMY_DATABASE_URL else {})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 from app.database import engine, Base
 from app.routers import destinations
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Travel Destinations API")
+
+@app.get("/")
+def root():
+    return RedirectResponse(url="/static/index.html")
 
 app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 app.include_router(destinations.router)
